998de024dff96c22f7c858b3f61260e878d11a6e
Meet Mandhane <meetmandhanemnm@gmail.com>
2023-04-22 01:05:33 +0530

    Added Service in activity1 FindByEmployee

--- src/main/java/com/example/spe_mini/Services/Activity1Services.java
+++ src/main/java/com/example/spe_mini/Services/Activity1Services.java
@@ -1,7 +1,12 @@
 package com.example.spe_mini.Services;
 
 import com.example.spe_mini.Models.Activity1;
+import com.example.spe_mini.Models.Employee;
+
+import java.util.List;
 
 public interface Activity1Services {
     Activity1 addActivity(Activity1 activity1);
+
+    List<Activity1> findActivityByEmployee(Employee emp);
 }
--- src/main/java/com/example/spe_mini/Services/Impl/Activity1ServicesImplementation.java
+++ src/main/java/com/example/spe_mini/Services/Impl/Activity1ServicesImplementation.java
@@ -1,10 +1,13 @@
 package com.example.spe_mini.Services.Impl;
 
 import com.example.spe_mini.Models.Activity1;
+import com.example.spe_mini.Models.Employee;
 import com.example.spe_mini.Repo.Activity1Repo;
 import com.example.spe_mini.Services.Activity1Services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class Activity1ServicesImplementation implements Activity1Services {
@@ -15,5 +18,11 @@ public class Activity1ServicesImplementation implements Activity1Services {
     public Activity1 addActivity(Activity1 activity1){
         Activity1 activity11=this.activity1Repo.save(activity1);
         return activity11;
+    }
+
+    @Override
+    public List<Activity1> findActivityByEmployee(Employee emp) {
+
+        return null;
     }
 }
